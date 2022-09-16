a56dc3fb97d9efc241c620303e36e2beb96a8a9d
LuizClaudio <cazarin.junior@gmail.com>
2022-09-16 14:42:01 -0300

    Update

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,29 @@
+import { View, Image } from "react-native";
+import { styles } from "./styles";
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { Heading } from "../../components/Heading";
+import { GameCard } from "../../components/GameCard";
+import { FlatList } from "react-native";
+
+import { GAMES } from "../../utils/games";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo}></Image>
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
+}
